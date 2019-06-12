fcbe9050339edbba7988ca9ba18bd9504236e374
Your Name <you@cdgit statusgit config --global user.email you@>
2019-06-12 04:13:00 +0300

    crud

--- 2.JavaCore/src/com/javarush/task/task17/task1710/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task17/task1710/Solution.java
@@ -1,5 +1,8 @@
 package com.javarush.task.task17.task1710;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -16,7 +19,21 @@ public class Solution {
         allPeople.add(Person.createMale("Петров Петр", new Date()));  //сегодня родился    id=1
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         //start here - начни тут
+        System.out.println(
+                "Доступны 4 команды: \n" +
+                        "\"-c\" - добавляет человека с заданными параметрами в конец allPeople, выводит id (index) на экран \n" +
+                        "\"-u\" - обновляет данные человека с данным id \n" +
+                        "\"-d\" - производит логическое удаление человека с id, заменяет все его данные на null \n" +
+                        "\"-i\" - выводит на экран информацию о человеке с id: name sex (м/ж) bd (формат 15-Apr-1990)\n" +
+                        "Введите команду:");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        allPeople.add(Person.createFemale(reader.readLine(),));
+
+
+
+
     }
 }
