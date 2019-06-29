3fc68b053d78930c83780e147874f7bb94431eea
PawExp <dactiloscopist@gmail.com>
2019-06-29 18:55:29 +0300

    task1821

--- 2.JavaCore/src/com/javarush/task/task18/task1821/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1821/Solution.java
@@ -4,8 +4,27 @@ package com.javarush.task.task18.task1821;
 Встречаемость символов
 */
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        FileInputStream fis = new FileInputStream(args[0]);
+        int [] charTypes = new int[255];
+        for(int s: charTypes){
+            s = 0;
+        }
+        while (fis.available()>0){
+            int c = fis.read();
+            for(int i = 0; i < charTypes.length; i++){
+                if (c == i) charTypes[i] = charTypes[i] + 1;
+            }
+        }
+        fis.close();
+        for (int i = 0; i < charTypes.length; i++){
+            if(charTypes[i] !=0) System.out.println((char)i + " " + charTypes[i]);
+        }
 
     }
 }
