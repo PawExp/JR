f93b68926fdc79b121ba47af8858303e2d4cd7a2
PawExp <dactiloscopist@gmail.com>
2019-07-13 17:51:45 +0300

    2002 solved

---
+++ 2.JavaCore/src/com/javarush/task/task20/task2002/Solution.java
@@ -0,0 +1,82 @@
+package com.javarush.task.task20.task2002;
+
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/* 
+Читаем и пишем в файл: JavaRush
+*/
+public class Solution {
+    public static void main(String[] args) {
+        //you can find your_file_name.tmp in your TMP directory or adjust outputStream/inputStream according to your file's actual location
+        //вы можете найти your_file_name.tmp в папке TMP или исправьте outputStream/inputStream в соответствии с путем к вашему реальному файлу
+        try {
+            File yourFile = File.createTempFile("test", null);
+            OutputStream outputStream = new FileOutputStream("test");
+            InputStream inputStream = new FileInputStream("test");
+
+            JavaRush javaRush = new JavaRush();
+            //initialize users field for the javaRush object here - инициализируйте поле users для объекта javaRush тут
+//            javaRush.users.add(new User(
+//                    "Ivan",
+//                    "Ivanov",
+//                    new SimpleDateFormat("dd.mm.YYYY").parse("13.07.2019"),
+//                    true,
+//                    User.Country.RUSSIA));
+            javaRush.save(outputStream);
+            outputStream.flush();
+
+            JavaRush loadedObject = new JavaRush();
+            loadedObject.load(inputStream);
+            //here check that the javaRush object is equal to the loadedObject object - проверьте тут, что javaRush и loadedObject равны
+            System.out.println(javaRush.equals(loadedObject));
+            outputStream.close();
+            inputStream.close();
+
+        } catch (IOException e) {
+            //e.printStackTrace();
+            System.out.println("Oops, something is wrong with my file");
+        } catch (Exception e) {
+            //e.printStackTrace();
+            System.out.println("Oops, something is wrong with the save/load method");
+        }
+    }
+
+    public static class JavaRush {
+        public List<User> users = new ArrayList<>();
+
+        public void save(OutputStream outputStream) throws Exception {
+            //implement this method - реализуйте этот метод
+                ObjectOutputStream oos = new ObjectOutputStream(outputStream);
+                oos.writeObject(this.users);
+                oos.close();
+        }
+
+        public void load(InputStream inputStream) throws Exception {
+            //implement this method - реализуйте этот метод
+            ObjectInputStream ois = new ObjectInputStream(inputStream);
+            Object o = ois.readObject();
+            this.users = (ArrayList<User>) o;
+            ois.close();
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            JavaRush javaRush = (JavaRush) o;
+
+            return users != null ? users.equals(javaRush.users) : javaRush.users == null;
+
+        }
+
+        @Override
+        public int hashCode() {
+            return users != null ? users.hashCode() : 0;
+        }
+    }
+}
