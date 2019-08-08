d4b8f9c225d8fb2ae0dfeef1fef245b3028c7a21
PawExp <dactiloscopist@gmail.com>
2019-08-08 20:16:34 +0300

    2011 solved

--- 2.JavaCore/src/com/javarush/task/task20/task2011/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task20/task2011/Solution.java
@@ -1,9 +1,7 @@
 package com.javarush.task.task20.task2011;
 
-import java.io.Externalizable;
-import java.io.IOException;
-import java.io.ObjectInput;
-import java.io.ObjectOutput;
+import java.io.*;
+import java.util.Objects;
 
 /*
 Externalizable для апартаментов
@@ -34,16 +32,53 @@ public class Solution {
 
         @Override
         public void writeExternal(ObjectOutput out) throws IOException {
-
+            out.writeObject(address);
+            out.writeInt(year);
         }
 
         @Override
         public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
-
+            address = (String)in.readObject();
+            year = in.readInt();
         }
+
+//        @Override
+//        public boolean equals(Object o) {
+//            if (this == o) return true;
+//            if (!(o instanceof Apartment)) return false;
+//            Apartment apartment = (Apartment) o;
+//            return year == apartment.year &&
+//                    address.equals(apartment.address);
+//        }
+//
+//        @Override
+//        public int hashCode() {
+//            return Objects.hash(address, year);
+//        }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
+
+       /* Apartment apartment = new Apartment();
+        apartment.address = "ADDRESS";
+        apartment.year = 2019;
+
+       try(FileOutputStream fos = new FileOutputStream("test");
+        ObjectOutputStream oos = new ObjectOutputStream(fos)) {
+           oos.writeObject(apartment);
+       } catch (IOException e) {
+           e.printStackTrace();
+       }
+
+       try(FileInputStream fis = new FileInputStream("test");
+       ObjectInputStream ois = new ObjectInputStream(fis)){
+           Apartment apartment1 = (Apartment)ois.readObject();
+           System.out.println(apartment1);
+           System.out.println(apartment.equals(apartment1));
+       } catch (IOException | ClassNotFoundException e) {
+           e.printStackTrace();
+       }
+       // System.out.println(apartment);*/
 
     }
 }
